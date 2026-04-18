3baf202a66797bcf2f0805fb163064330b7e18eb
NivAmir <nivam2005@gmail.com>
2026-04-18 14:15:13 +0300

    Update dashboard.js NEW CAMIP

--- js/dashboard.js
+++ js/dashboard.js
@@ -35,7 +35,7 @@ let aiModel;
 let pressureChart;
 
 /** Local IP for ESP32-CAM stream. */
-window.CAMERA_IP = "172.20.10.8";
+window.CAMERA_IP = "10.122.154.110";
 
 /** * Stores the most recent material identified by the AI model.
  * Defaults to "None" until a successful classification occurs.
